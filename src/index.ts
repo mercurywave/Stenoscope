dbf14c853a1fecad21011fe7e99207ddc48ac74c
Alex Nelson <alex.nelson@mercury-wave.com>
2025-03-22 19:39:28 -0500

    actually processes audio, though not in real time

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,43 @@
+import { AudioAnalyzer } from "./audio analyzer";
+import { Deferred } from "./util";
 
 document.addEventListener("DOMContentLoaded", () => {
     setup();
 });
 
+enum eRecordingState { Idle, Loading, Paused , Recording}
+enum eLoadState { Null, Loading, Loaded }
+
 let _worker: Worker;
+let _recordingStatus: eRecordingState = eRecordingState.Idle;
+
+let _mediaRecorder: MediaRecorder;
+let _audioChunks: Blob[] = [];
 
 async function setup() {
+    
+    updateStatus(eRecordingState.Idle);
 
     _worker = new Worker(new URL("./transcribe worker.ts", import.meta.url), { type: "module" });
 
     _worker.addEventListener("message", (e: MessageEvent) => {
-        const { type, data } = e.data;
+        const { status, data } = e.data;
 
-        switch (type) {
+        switch (status) {
             case 'ready':
                 console.log("Worker loaded");
+                initAudio().then(() => {
+                    updateStatus(eRecordingState.Paused);
+                    btRecord.click();
+                });
                 break;
 
             case 'generate':
                 console.log("Generated:", data);
+                break;
+
+            case 'update':
+                console.log("Updated:", data);
                 break;
         }
     });
@@ -26,62 +45,91 @@ async function setup() {
     let btRecord = document.getElementById("btRecord");
     let outputDiv = document.getElementById("output");
 
-    let isRecording = false;
-    let mediaRecorder: MediaRecorder;
-    let audioChunks: Blob[] = [];
-
     btRecord.addEventListener("click", async () => {
-        if (!isRecording) {
+        if(_recordingStatus == eRecordingState.Idle){
+            updateStatus(eRecordingState.Loading);
             _worker.postMessage({ type: "load" });
-            // Start recording
-            isRecording = true;
-            btRecord.textContent = "⏸"; // Update button state
-
-            const stream = await navigator.mediaDevices.getUserMedia({
-                audio: {
-                    sampleRate: 16000, // Set sample rate to 16kHz for ASR
-                    channelCount: 1,
-                    echoCancellation: true,
-                    noiseSuppression: true,
-                }
-            });
-            mediaRecorder = new MediaRecorder(stream);
-
-            mediaRecorder.ondataavailable = (event) => {
-                audioChunks.push(event.data);
-                const blob = new Blob([event.data], { type: this.recorder!.mimeType });
-                const fileReader = new FileReader();
-                fileReader.onloadend = async () => {
-                    try {
-                        const arrayBuffer = fileReader.result;
-
-                        const decoded = await this.audioContext.decodeAudioData(arrayBuffer as ArrayBuffer);
-                        const channelData = decoded.getChannelData(0);
-
-                        const analysis = this.audioAnalyzer.analyzeAudioData(channelData);
-                        if (!analysis.hasSpeech) {
-                            return;
-                        }
-
-                        this.worker.postMessage({ type: 'generate', data: { audio: channelData, language: 'english' } });
-                    } catch (e) {
-                        console.error('Error decoding audio data:', e);
-                    }
-                }
-                fileReader.readAsArrayBuffer(blob);
-            };
-
-            mediaRecorder.onstop = async () => {
-                audioChunks = []; // Clear chunks for next recording
-            };
-
-            mediaRecorder.start();
-        } else {
-            // Stop recording
-            isRecording = false;
-            btRecord.textContent = "⏹"; // Update button state
-            mediaRecorder.stop();
+        }
+        else if (_recordingStatus == eRecordingState.Paused) {
+            _mediaRecorder.start();
+        } else if (_recordingStatus == eRecordingState.Recording) {
+            _mediaRecorder.stop();
         }
     });
 }
 
+let _mediaInit: Deferred<void> = null;
+async function initAudio(){
+    if(_mediaInit) {
+        await _mediaInit;
+        return;
+    }
+    _mediaInit = new Deferred();
+    const stream = await navigator.mediaDevices.getUserMedia({
+        audio: {
+            sampleRate: 16000, // Set sample rate to 16kHz for ASR
+            channelCount: 1,
+            echoCancellation: true,
+            noiseSuppression: true,
+        }
+    });
+    _mediaRecorder = new MediaRecorder(stream);
+    let audioContext = new AudioContext({sampleRate: 16000});
+    let audioAnalyzer = new AudioAnalyzer({sampleRate: audioContext.sampleRate});
+
+    _mediaRecorder.onstart = (event) => {
+        updateStatus(eRecordingState.Recording);
+    };
+    
+    _mediaRecorder.ondataavailable = (event) => {
+        console.log("asdfer");
+        _audioChunks.push(event.data);
+        const blob = new Blob([event.data], { type: _mediaRecorder!.mimeType });
+        const fileReader = new FileReader();
+        fileReader.onloadend = async () => {
+            try {
+                const arrayBuffer = fileReader.result;
+
+                const decoded = await audioContext.decodeAudioData(arrayBuffer as ArrayBuffer);
+                const channelData = decoded.getChannelData(0);
+
+                const analysis = audioAnalyzer.analyzeAudioData(channelData);
+                console.log("checking for speech");
+                if (!analysis.hasSpeech) {
+                    console.log("no speech");
+                    //return;
+                }
+
+                _worker.postMessage({ type: 'generate', data: { audio: channelData, language: 'english' } });
+            } catch (e) {
+                console.error('Error decoding audio data:', e);
+            }
+        }
+        fileReader.readAsArrayBuffer(blob);
+    };
+
+    _mediaRecorder.onstop = async () => {
+        _audioChunks = []; // Clear chunks for next recording
+        updateStatus(eRecordingState.Paused);
+    };
+    _mediaInit.resolve();
+}
+
+function updateStatus(status: eRecordingState){
+    _recordingStatus = status;
+    let divStatus = document.getElementById("recState");
+    switch(status){
+        case eRecordingState.Idle:
+            divStatus.textContent = "⏹";
+            break;
+        case eRecordingState.Loading:
+            divStatus.textContent = "⋯";
+            break;
+        case eRecordingState.Paused:
+            divStatus.textContent = "⏺";
+            break;
+        case eRecordingState.Recording:
+            divStatus.textContent = "⏸";
+            break;
+    }
+}
--- src/transcribe worker.ts
+++ src/transcribe worker.ts
@@ -1,12 +1,14 @@
-import { AutoProcessor, AutoTokenizer, PreTrainedModel, PreTrainedTokenizer, Processor, WhisperForConditionalGeneration } from "@xenova/transformers";
+import { AutoProcessor, AutoTokenizer, MoonshineForConditionalGeneration, pipeline, PreTrainedModel, PreTrainedTokenizer, Processor, TextStreamer } from "@huggingface/transformers";
 
 let _manager: Manager;
 class Manager {
-    _modelId = "onnx-community/whisper-base";
+    _modelId = "onnx-community/moonshine-tiny-ONNX";
     _tokenizer: PreTrainedTokenizer = null;
     _processor: Processor = null;
     _model: PreTrainedModel = null;
     _processsing = false;
+    _loaded = false;
+    //_instance: AutomaticSpeechRecognitionPipeline = null;
 
     public async setup() {
         this._tokenizer = await AutoTokenizer.from_pretrained(this._modelId, {
@@ -15,30 +17,65 @@ class Manager {
         this._processor = await AutoProcessor.from_pretrained(this._modelId, {
             progress_callback: progressCallback
         });
-        this._model = await WhisperForConditionalGeneration.from_pretrained(this._modelId, {
+        this._model = await MoonshineForConditionalGeneration.from_pretrained(this._modelId, {
             progress_callback: progressCallback,
             dtype: {
                 encoder_model: 'fp32', // 'fp16' works too
-                decoder_model_merged: 'q4', // or 'fp32' ('fp16' is broken)
+                decoder_model_merged: 'q4',
             },
             device: 'webgpu',
         } as any); // seems like there are parameters that aren't in the type defs
+        // this._instance = await pipeline("automatic-speech-recognition", this._modelId, {
+        //     progress_callback: progressCallback,
+        //     dtype: {
+        //         encoder_model: 'fp32', // 'fp16' works too
+        //         decoder_model_merged: 'q4', // or 'fp32' ('fp16' is broken)
+        //     },
+        //     device: 'webgpu',
+        // } as any); // seems like there are parameters that aren't in the type defs
         self.postMessage({ status: 'ready' });
+        this._loaded = true;
     }
 
+    // @ts-ignore
     public async run({ audio, language }) {
-        if (this._processsing) return;
+        if (!this._loaded || this._processsing) return;
         this._processsing = true;
         self.postMessage({ status: 'start' });
 
+        let startTime = performance.now();
+        let numTokens = 0;
+        const callbackFunction = (output) => {
+            let tps;
+            if (numTokens++ > 0) {
+                tps = numTokens / (performance.now() - startTime) * 1000;
+            }
+            self.postMessage({
+                status: 'update',
+                output, tps, numTokens,
+            });
+        }
+
+        const streamer = new TextStreamer(this._tokenizer, {
+            skip_prompt: true,
+            decode_kwargs: {
+                skip_special_tokens: true,
+            },
+            skip_special_tokens: true,
+            callback_function: callbackFunction,
+        });
+
         const inputs = await this._processor(audio);
-        const outputs = await this._model.generate(inputs,
+        const outputs = await this._model.generate(
             {
+                ...inputs,
                 max_new_tokens: 64,
                 language,
-            }
+                streamer,
+            } //as any // seems like there are parameters that aren't in the type defs
         );
-        const outputText = this._tokenizer.batch_decode(outputs, { skip_special_tokens: true });
+        const outputText = this._tokenizer.batch_decode(outputs as any, { skip_special_tokens: true });
+        console.log(outputText);
 
         self.postMessage({
             status: 'generate',
@@ -50,7 +87,7 @@ class Manager {
 }
 
 function progressCallback(msg) {
-    console.log(msg);
+    //console.log(msg);
 }
 
 self.addEventListener('message', async (e: MessageEvent) => {
